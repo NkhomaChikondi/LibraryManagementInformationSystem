85b94dadc8477122ddaf0eb5fa5b95a5ad5f8fb9
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-11-24 09:19:09 +0200

    Add Repository

---
+++ LMIS.Api.Core/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMIS.Api.Core.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter = null);
+        IQueryable<T> GetAllAsync();
+        IQueryable<T> Get(Expression<Func<T, bool>> filter);
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(object id);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(Expression<Func<T, bool>> filter = null);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> filter);
+
+    }
+}
