81af4c7ad0fb3c229cedd681fadf64fb47041c12
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-11-24 10:30:50 +0200

    Add User,Role,UserroleDTO

---
+++ LMIS.Api.Core/DTOs/RoleDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMIS.Api.Core.DTOs
+{
+    public class RoleDTO
+    {
+        public int RoleId { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
