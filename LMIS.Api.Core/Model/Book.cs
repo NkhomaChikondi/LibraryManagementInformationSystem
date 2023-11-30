a02a304fe8acbdbe618e29b90c803f2e6abd2fc1
Chikondi Nkhoma <61491585+NkhomaChikondi@users.noreply.github.com>
2023-11-30 11:10:14 +0200

    Add book Model

---
+++ LMIS.Api.Core/Model/Book.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMIS.Api.Core.Model
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string State { get; set; }
+        public string ObtainedThrough { get; set; }
+        public string Publisher { get; set; }
+        public string Status { get; set; }
+        public string Genre { get; set; }        
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
